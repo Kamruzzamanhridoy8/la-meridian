2cd5a63b610d454f574df0a613c8a60d15c5a2fc
Kamruzzamanhridoy8 <Kamruzzamanhridoy@hotmail.com>
2023-01-23 07:22:52 +0600

    js fucntionality

--- js/bank.js
+++ js/bank.js
@@ -13,3 +13,27 @@ document.getElementById('deposite-button').addEventListener('click',function(){
 
     depositeAmount.value ='';
 })
+
+document.getElementById('transfer-button').addEventListener('click',function(){
+    const getPayment = document.getElementById('payment-amount');
+    const payment= getPayment.value;
+
+    const previuseTotal = document.getElementById('total-balance');
+    const newTotal = parseFloat(previuseTotal.innerText);
+
+    if (payment > newTotal) {
+        alert('insaficiant balance')
+        getPayment.value='';
+        return;
+    }
+    
+    const paymentAmount = document.getElementById('pre-payment-amount');
+    const totalPayment= parseFloat(paymentAmount.innerText) + parseFloat(payment);
+    paymentAmount.innerText = totalPayment;
+
+    const previusePayment = document.getElementById('total-balance');
+    const newPayment = parseFloat(previusePayment.innerText) - parseFloat(payment);
+    previusePayment.innerText = newPayment; 
+    
+    getPayment.value='';
+})
